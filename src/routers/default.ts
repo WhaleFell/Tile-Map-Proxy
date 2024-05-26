c7878823bf46face443d971840adb470f537b03c
whalefell <63758849+WhaleFell@users.noreply.github.com>
2024-05-26 11:22:18 +0800

    Fix: proxy url vaildator

--- src/routers/default.ts
+++ src/routers/default.ts
@@ -1,6 +1,7 @@
 // default router
 import { Hono } from 'hono'
 import { getRuntimeKey } from 'hono/adapter'
+import { validator } from 'hono/validator'
 import { StatusCode } from 'hono/utils/http-status'
 import { delHeaderLengthMiddleware, customFetch } from '../utils'
 
@@ -14,8 +15,24 @@ defaultRoute.get('/runtime', (c) => {
 	return c.json({ runtime: getRuntimeKey() })
 })
 
-defaultRoute.all('/proxy/:url{.*?}', delHeaderLengthMiddleware, async (c) => {
-	const url = c.req.param('url')
+const URL_REGEX = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/
+
+const validatorURLMiddleware = validator('query', (value, c) => {
+	if (typeof value.url !== 'string') {
+		return c.json({ error: `Invalid URL ${value.url}` }, 400)
+	}
+
+	if (!URL_REGEX.test(value.url)) {
+		return c.json({ error: `Invalid URL ${value.url}` }, 400)
+	}
+
+	return {
+		url: value.url,
+	}
+})
+
+defaultRoute.all('/proxy', validatorURLMiddleware, delHeaderLengthMiddleware, async (c) => {
+	const url = c.req.valid('query').url
 	console.log(`[defaultRoute] Fetching data from ${url}...`)
 	const rep = await customFetch(url, { headers: c.req.raw.headers, method: c.req.method })
 	return c.newResponse(rep.body, rep.status as StatusCode, Object.fromEntries(rep.headers))
