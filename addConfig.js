f8170ba3923dff2208bc56ac4c375aa5b0a1b93d
whalefell <63758849+WhaleFell@users.noreply.github.com>
2024-06-27 03:04:04 +0800

    🐞 fix: vercel bug,Dynamic require of "assert" [esbuild #1921](https://github.com/evanw/esbuild/issues/1921#issuecomment-1898197331)

--- addConfig.js
+++ addConfig.js
@@ -1,15 +1,25 @@
-import * as fs from 'fs'
+import * as fs from "fs"
 
 // check if the file path is provided
 if (process.argv.length < 3) {
-	console.log('Offer a file path as argument to add Vercel config, Please!')
-	process.exit(1)
+  console.log("Offer a file path as argument to add Vercel config, Please!")
+  process.exit(1)
 }
 
 const filePath = process.argv[2]
 
 // the content to add in the file
 const contentToAdd = `
+// https://github.com/evanw/esbuild/issues/1921#issuecomment-1898197331
+
+import { createRequire } from 'node:module';
+import path from 'node:path';
+import url from 'node:url';
+
+globalThis.require = createRequire(import.meta.url);
+globalThis.__filename = url.fileURLToPath(import.meta.url);
+globalThis.__dirname = path.dirname(__filename);
+
 export const config = {
 	supportsResponseStreaming: true,
 	api: {
@@ -20,19 +30,19 @@ export const dynamic = 'force-dynamic'
 
 `
 
-fs.readFile(filePath, 'utf8', (err, data) => {
-	if (err) {
-		console.error(`Cannot read file: ${err.message}`)
-		process.exit(1)
-	}
-	const newData = contentToAdd + data
+fs.readFile(filePath, "utf8", (err, data) => {
+  if (err) {
+    console.error(`Cannot read file: ${err.message}`)
+    process.exit(1)
+  }
+  const newData = contentToAdd + data
 
-	fs.writeFile(filePath, newData, 'utf8', (err) => {
-		if (err) {
-			console.error(`Cannot write file: ${err.message}`)
-			process.exit(1)
-		}
+  fs.writeFile(filePath, newData, "utf8", (err) => {
+    if (err) {
+      console.error(`Cannot write file: ${err.message}`)
+      process.exit(1)
+    }
 
-		console.log('Add Vercel config in compiled JS file successfully!')
-	})
+    console.log("Add Vercel config in compiled JS file successfully!")
+  })
 })
